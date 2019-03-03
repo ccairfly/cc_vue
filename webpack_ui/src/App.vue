c24f0e2db0e6bb38387550772d0cda81c100fe1d
cc_airfly <921537102@qq.com>
2019-03-03 23:13:44 +0800

    test:mint-ui Toast

--- webpack_ui/src/App.vue
+++ webpack_ui/src/App.vue
@@ -1,7 +1,7 @@
 <template>
         <div>
             <h3>{{ msg }}</h3>
-            <mt-button size="small" type="primary">small button</mt-button>
+            <mt-button size="small" type="primary" @click="show">small button</mt-button>
             <router-link to="/" tag="button">back</router-link>
             <router-link to="/account" tag="button">进入账户</router-link>
             <router-link to="/goodslist" tag="button">进入商品</router-link>
@@ -10,18 +10,30 @@
 </template>
 
 <script>
+import { Toast } from 'mint-ui';
 // 向外暴露这个对象
 export default {
     data() {
         return {
             // 暂时不可用插值表达式
             msg : "hello webpack vue App--cc",
-            msg2 : "这是一个寂寞的天,下着有些伤心的雨"
+            msg2 : "这是一个寂寞的天,下着有些伤心的雨",
+            toastInstance : null,
         }
     },
     methods: {
         show(){
-            console.log("this is App.vue show method");
+            this.toastInstance = Toast({
+                message: '这是一个寂寞的天',
+                position: 'middle',
+                duration: -1
+                });
+            this.removeToastDelay()
+        },
+        removeToastDelay(){
+            setTimeout(() => {
+                this.toastInstance.close()
+            }, 3000);
         }
     },
 }
--- webpack_ui/webpack-vue-ui.md
+++ webpack_ui/webpack-vue-ui.md
@@ -1,3 +1,25 @@
 1.在vue中使用mint-ui:
 *工程中安装mint-ui到项目依赖npm i mint-ui -S
 *在main.js中导入mint-ui包 和mint-ui的样式
+*注意js组件一般都需要导入,css组件若全局导入了,则可以不需要再次导入
+*Toast js组件的使用:导包import { Toast } from 'mint-ui' 和使用
+Toast({
+  message: '提示',
+  position: 'bottom',
+  duration: 5000
+});
+*按需引入:需要借助babel-plugin-component
+npm install babel-plugin-component -D
+在.babelrc中添加plugin
+["component", [
+    {
+      "libraryName": "mint-ui",
+      "style": true
+    }
+]]
+按需导入button和cell并注册组件
+import { Button, Cell } from 'mint-ui'
+Vue.component(Button.name, Button)
+Vue.component(Cell.name, Cell)
+
+2.使用MUI,MUI与bootstrap类似使用与任何项目,而mint-ui是使用vue技术封装出来的成套的组件,可以无缝和VUE项目进行开发
